4fe1c0f592a77da1afd2e3f787b11ba35056d06b
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-05 13:11:21 +0300

    added a coding example.

--- java_script_learning/byyavuzyigit/Objects.js
+++ java_script_learning/byyavuzyigit/Objects.js
@@ -70,5 +70,49 @@ console.log(phoneticLookup("charlie"));
 console.log(testObj.hasOwnProperty("shirt"));
 console.log(testObj.hasOwnProperty("car"));
 
+// profile lookup example
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristan",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name, prop){
+    for(var i=0; i<contacts.length; i++){
+        if(contacts[i].firstName === name){
+            return contacts[i][prop] || "No such property.";
+        }
+    }
+    return "No such contact.";
+}
+
+console.log(lookUpProfile("Sherlock","likes"));
+console.log(lookUpProfile("Sherlock","cars"));
+console.log(lookUpProfile("Robert","likes"));
+
+
+
 
 
