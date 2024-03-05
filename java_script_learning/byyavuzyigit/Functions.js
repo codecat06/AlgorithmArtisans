782ab480f89d43d890de41c431358f838b0bb232
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-05 13:10:43 +0300

    added Math functions, parseInt and arrow functions.

--- java_script_learning/byyavuzyigit/Functions.js
+++ java_script_learning/byyavuzyigit/Functions.js
@@ -62,3 +62,38 @@ var testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+// random decimal number generator
+console.log(Math.random()); // generates decimal numbers between 0 and 1 (can be 0, cannot be 1)
+console.log(Math.floor(Math.random() * 20)); // generates whole numbers (in this case 0-19)
+
+// parseInt function
+console.log(parseInt("56") + parseInt("24"));
+// turning string to an int with a base
+console.log(parseInt("10011",2)); // returns 19
+
+// arrow functions
+const magic = () => 10; // you can write return 10 too
+console.log(magic()); // returns 10
+
+const arrConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(arrConcat([1, 2], [3, 4, 5]));
+
+// filter and map functions
+const realNumbers = [4, 5.6, -9.8, -3, 42, 6, 9.34, -2];
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x*x);
+    return squaredIntegers;
+}
+console.log(squareList(realNumbers));
+
+// rest operator = ...
+const sum = (function(){
+    return function sum(... args){
+        return args.reduce((a,b) => a + b ,0);
+    };
+})();
+console.log(sum(1,2,3));
+console.log(sum(1,2,3,4,5));
+
+
