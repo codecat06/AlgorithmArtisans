de31e081ca811d0b3a8966cfc784fa6392c17dd9
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-06 17:44:47 +0300

    updated with spread operator and destructuring assignment.

--- java_script_learning/byyavuzyigit/Arrays.js
+++ java_script_learning/byyavuzyigit/Arrays.js
@@ -33,3 +33,34 @@ console.log(beginRemove);
 // unshift() adds an element to the beginning of the array
 nestedArray.unshift(beginRemove);
 console.log(nestedArray);
+
+// spread operator = ...
+// copies the content of an array
+const arr1 = ["a", "b", "c", "d"];
+let arr2;
+arr2 = arr1;
+arr1[0] = "0";
+console.log(arr2); // changed a to 0 because it did not copy the contents of arr1
+let arr3;
+arr1[0] = "a";
+arr3 = [...arr1];
+arr1[0] = "0";
+console.log(arr3); // copied arr1
+
+// destructuring assignment
+const [z,x, , y] =[1, 2, 3, 4, 5, 6]; // you cannot directly assign wanted index
+                                                                // however you can put commas to reach the
+                                                                // wanted index.
+console.log(z,x,y); // 1 2 4
+
+// switching variables values
+let a = 6; let b = 8;
+[a,b] = [b,a];
+console.log(a,b); // 8 6
+
+// using rest operator with spread operator
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+const [ , , ...array2] = array1; // removes first two elements of array1
+console.log(array2);
+
+
