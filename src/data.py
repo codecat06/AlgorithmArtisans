9569f5f6793c67ad5ca7148c7c3893ee1ce60c82
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-11 22:25:30 +0300

    storing api data in a text file. working on reading the data from the file with a matching callsign.

--- src/data.py
+++ src/data.py
@@ -1,5 +1,36 @@
 from opensky_api import OpenSkyApi
+import ast
+
 api = OpenSkyApi()
 states = api.get_states()
-for s in states.states:
-    print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+
+# for s in states.states:
+#    print(s)
+
+# saving plane data in the "planes.txt" file
+with open("planes.txt", "a") as fo:
+    for s in states.states:
+        fo.write(str(s))
+
+callsign = "ANZ006L"
+# reading the "planes.txt" file to find the data matching with the callsign
+
+callsign_to_match = "ANZ006L"
+
+# with open("planes.txt", "r") as fo:
+#     for line in fo:
+#         # Convert the line to a dictionary
+#         try:
+#             plane_info = ast.literal_eval(line.strip())
+#         except SyntaxError:
+#             # Skip lines that are not valid dictionaries
+#             continue
+#         # Check if the callsign matches
+#         if plane_info.get('callsign') == callsign_to_match:
+#             # Print the plane information in the desired format
+#             print("{")
+#             for key, value in plane_info.items():
+#                 print(f"    '{key}': {value},")
+#             print("}")
+
+
