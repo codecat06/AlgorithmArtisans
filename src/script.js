627e655ed2e3e4c621439b3fe05f32725d1ca8e2
sasalya98 <70776388+sasalya98@users.noreply.github.com>
2024-03-18 15:53:31 +0300

    Updated script.js and PlaneData.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import { loadPlanesDataFromFile, findCallsigns, findMatchingCallsignData, printMatchingCallsignData } from'./PlaneData.js'
+
 let callsigns;
 var selectedCallsign = "NOT SELECTED";
 // script.js
@@ -79,56 +81,10 @@ function printOnScreen(content) {
 
 //displayPlanes(planes);
 
-async function loadPlanesDataFromFile(filename) {
-
-    const response = await fetch(filename);
-  
-    const text = await response.text();
-  
-    const planesData = text.split('\n').filter(line => line.trim().length > 0).map(JSON.parse);
-  
-    return planesData;
-  
-}
-
-function findMatchingCallsignData(planesData, callsign) {
-    return planesData.filter(planeData => planeData.callsign.trim() === callsign.trim());
-}
-function findCallsigns(planesData) {
-    const callsigns = new Set();
-    planesData.forEach(planeData => {
-        callsigns.add(planeData.callsign.trim());
-    });
-    return [...callsigns];
-}
 
 
 
-function printMatchingCallsignData(matchingData) {
-    matchingData.forEach(data => {
-        console.log(`baro_altitude: ${data.baro_altitude}`);
-        console.log(`category: ${data.category}`);
-        console.log(`geo_altitude: ${data.geo_altitude}`);
-        console.log(`icao24: ${data.icao24}`);
-        console.log(`last_contact: ${data.last_contact}`);
-        console.log(`latitude: ${data.latitude}`);
-        console.log(`longitude: ${data.longitude}`);
-        console.log(`on_ground: ${data.on_ground}`);
-        console.log(`origin_country: ${data.origin_country}`);
-        console.log(`position_source: ${data.position_source}`);
-        console.log(`sensors: ${data.sensors}`);
-        console.log(`spi: ${data.spi}`);
-        console.log(`squawk: ${data.squawk}`);
-        console.log(`time_position: ${data.time_position}`);
-        console.log(`true_track: ${data.true_track}`);
-        console.log(`velocity: ${data.velocity}`);
-        console.log(`vertical_rate: ${data.vertical_rate}`);
 
-        // converting string values to number values
-        console.log(`geo_altitude as number: ${Number(data.geo_altitude)}`)
-        console.log("");
-    });
-}
 
 // Usage: Load the data from file and process it
 loadPlanesDataFromFile('planes.txt')
@@ -145,7 +101,7 @@ loadPlanesDataFromFile('planes.txt')
 loadPlanesDataFromFile("planes.txt")
 .then(planesData => {
     callsigns = findCallsigns(planesData);
-    console.log(callsigns);
+    //console.log(callsigns);
     //displayPlanes(callsigns);
     displayCallsigns(callsigns);
     //filterCallsigns(planeInput);
